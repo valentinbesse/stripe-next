25cae8b8f1267019dbdd800fe370659bf29d6763
Valentin Besse <valentinbesse@aumbox.net>
2024-11-04 22:04:25 +0100

    Default setup for the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui';
+import tailwindTypography from '@tailwindcss/typography';
 
 const config: Config = {
   content: [
@@ -14,6 +16,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindTypography,
+    daisyui
+  ],
 };
 export default config;
