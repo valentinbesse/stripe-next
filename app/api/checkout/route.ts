95ef1b39fef461835b56ca5165b35c52467cb955
Valentin Besse <valentinbesse@aumbox.net>
2024-11-06 22:48:34 +0100

    Correction on one-time-fee price_id

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { stripe } from '@/utils/stripe';
+
+export async function POST(request: Request) {
+    try {
+        const { priceId, email, userId } = await request.json();
+
+        const session = await stripe.checkout.sessions.create({
+            metadata: {
+                user_id: userId,
+            },
+            customer_email: email,
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    // base subscription
+                    price: priceId,
+                },
+                {
+                    price: 'price_1QIH2LB6XgI4BO0AIREisdSA',
+                    quantity: 1,
+                },
+            ],
+            mode: 'subscription',
+            success_url: `${request.headers.get('origin')}/success`,
+            cancel_url: `${request.headers.get('origin')}/cancel`,
+        });
+
+        return NextResponse.json({ id: session.id });
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
